3434b0259ebbfadc9a128b721d6049c27e96f610
Cuongdx77 <111421491+Cuongdx77@users.noreply.github.com>
2023-12-15 10:45:26 +0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,6 @@
-def runPythonScript() {
-    sh 'python Jenkins-lib/resources/test.py'
-}
+  def runMyPython() {
+    final pythonContent = libraryResource('test.py')
+    // There are definitely better ways to do this without having to write to the consumer's workspace
+    writeFile(file: 'test.py', text: pythonContent)
+    sh('python ./test.py')
+  }
