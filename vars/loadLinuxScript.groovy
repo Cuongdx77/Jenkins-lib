6bd82c935a0b9f9def21c2e4085085ab429b2237
Cuongdx77 <111421491+Cuongdx77@users.noreply.github.com>
2023-12-25 16:32:35 +0700

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "com/sharelib"    
+  def scriptcontents = libraryResource "com/sharelib/${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
   sh "chmod a+x ./${config.name}"
 }
