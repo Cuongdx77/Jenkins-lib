92c547cf94803627b12f9cfefcdb719374d0aac9
Cuongdx77 <111421491+Cuongdx77@users.noreply.github.com>
2023-12-25 17:11:19 +0700

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
   def scriptcontents = libraryResource "com/sharelib/${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
-  sh "python ./${config.name}"
+  sh "python3 ./${config.name}"
 } 
