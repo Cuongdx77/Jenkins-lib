aeb2fd612737281594b7d97fad46195bf4399319
Cuongdx77 <111421491+Cuongdx77@users.noreply.github.com>
2023-12-25 10:57:00 +0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,6 @@
   def call() {
-    libraryResource "/com/sharelib/test.sh"
+    def content = libraryResource "/com/sharelib/test.sh"
+      writerFile file: "bash_Script.sh" , text: content
+      sh "ls -a bash_Script.sh"
+      sh "bash ./bash_Script.sh"
   }
