0ef04a6749cc9457e39b1d5da062d214ecb03d05
Cuongdx77 <111421491+Cuongdx77@users.noreply.github.com>
2023-12-25 09:33:06 +0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,5 @@
   def call(Map config = [:]) {
-    def pythonContent = libraryResource "com/sharedlib/test.py'"
-    writeFile file: "my_file.py", text: pythonContent
-    sh "python ./my_file.py"
+    def scriptContent = libraryResource "com/sharedlib/${config.name}'"
+    writeFile file: "${config.name}", text: scriptContent
+    sh "chmod a+x ./${config.name}"
   }
