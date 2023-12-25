bfe459574dbdee2e444ac2f7bb4d512191ba8369
Cuongdx77 <111421491+Cuongdx77@users.noreply.github.com>
2023-12-25 10:59:17 +0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,5 +2,5 @@
     def content = libraryResource "/com/sharelib/test.sh"
       writerFile file: "bash_Script.sh" , text: content
       sh "ls -a bash_Script.sh"
-      sh "bash ./bash_Script.sh"
+      sh "chmod a+x ./bash_Script.sh"
   }
